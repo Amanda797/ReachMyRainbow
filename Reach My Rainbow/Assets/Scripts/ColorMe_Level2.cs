23da936394048e22bd03ad1de4277c22d2e6c120
AubreyDahl <and_dahl@yahoo.com>
2020-11-20 18:16:39 -0700

    Push = 11/20/2020

---
+++ Reach My Rainbow/Assets/Scripts/ColorMe_Level2.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+public class ColorMe_Level2 : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+
+    public Sprite Red;
+    public Sprite Yellow;
+    public Sprite Blue;
+    public Sprite Black;
+    public Sprite White;
+    public Sprite Green;
+    public Sprite Orange;
+    public Sprite Purple;
+
+    AudioSource audioData;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioData = GetComponent<AudioSource>();
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (GameManager.instance.selectedColor != null)
+        {
+            audioData.Play(0);
+            switch (GameManager.instance.selectedColorType)
+            {
+                case GameManager.colorType.Black:
+                    spriteRenderer.sprite = Black;
+                    break;
+                case GameManager.colorType.White:
+                    spriteRenderer.sprite = White;
+                    break;
+                case GameManager.colorType.Red:
+                    spriteRenderer.sprite = Red;
+                    break;
+                case GameManager.colorType.Blue:
+                    spriteRenderer.sprite = Blue;
+                    break;
+                case GameManager.colorType.Yellow:
+                    spriteRenderer.sprite = Yellow;
+                    break;
+                case GameManager.colorType.Green:
+                    spriteRenderer.sprite = Green;
+                    break;
+                case GameManager.colorType.Orange:
+                    spriteRenderer.sprite = Orange;
+                    break;
+                case GameManager.colorType.Purple:
+                    spriteRenderer.sprite = Purple;
+                    break;
+                case GameManager.colorType.None:
+                    break;
+
+            }
+        }
+    }
+}
--- Reach My Rainbow/Assets/Scripts/Level 1 Scripts/ColorVerification.cs
+++ Reach My Rainbow/Assets/Scripts/Level 1 Scripts/ColorVerification.cs
@@ -14,7 +14,15 @@ public class ColorVerification : MonoBehaviour
 
     public AudioClip Right;
 
+    public AudioClip RightVoice;
+
     public AudioClip Wrong;
+
+    public AudioClip WrongVoice;
+
+    public float rightPauseTime = 2; //Set delay time to allow first clip to finish before right clip
+    public float wrongPauseTime = 2; //Set delay time to allow first clip to finish before wrong clip
+
 
     public void OnMouseDown()
     {
@@ -25,15 +33,40 @@ public class ColorVerification : MonoBehaviour
             audiosource.clip = Right;
             audiosource.Play();
 
+
             GameManager.instance.toContinue++;
 
-            
+            StartCoroutine(RightPause()); //Pauses script then plays correct clip
+
+
         }
 
         else
         {
             audiosource.clip = Wrong;
             audiosource.Play();
+
+            StartCoroutine(WrongPause()); //Pauses script then plays the wrong clip
         }
+    }
+
+    //Coroutine to allow a delay audio between ding and right sound
+    IEnumerator RightPause()
+    {
+        yield return new WaitForSeconds(rightPauseTime);
+
+        //Changes clip to right voice then plays
+        audiosource.clip = RightVoice;
+        audiosource.Play();
+    }
+
+    //Coroutine to allow a delay audio between ding and wrong sound
+    IEnumerator WrongPause()
+    {
+        yield return new WaitForSeconds(wrongPauseTime);
+
+        //Changes clip to wrong voice then plays
+        audiosource.clip = WrongVoice;
+        audiosource.Play();
     }
 }
